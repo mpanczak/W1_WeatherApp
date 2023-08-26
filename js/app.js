5d2d24b1ce9e026348af8a80f77273b4549296f3
mpanczak <panczakmateusz@gmail.com>
2023-08-26 17:54:18 +0200

    first fetch

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const apiKey = "e07d5dbab7644d46beb153206232608";
+document.addEventListener("DOMContentLoaded", function (){
+    fetch("http://api.weatherapi.com/v1/forecast.json?key=" + apiKey + "&q=auto:ip&days=5")
+        .then(resp => resp.json().then(obj => console.log(obj))).catch(err => console.error(err));
+
+});
