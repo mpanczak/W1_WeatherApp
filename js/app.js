facb4157aa0fdc2b355bc8125e88b9508229aeda
mpanczak <panczakmateusz@gmail.com>
2023-08-26 18:03:19 +0200

    hide search module

--- js/app.js
+++ js/app.js
@@ -3,8 +3,15 @@ document.addEventListener("DOMContentLoaded", function (){
     fetch("http://api.weatherapi.com/v1/forecast.json?key=" + apiKey + "&q=auto:ip&days=5")
         .then(resp => resp.json().then(obj => console.log(obj))).catch(err => console.error(err));
 
+    // show search module
     document.getElementById("add-city").addEventListener("click", function (ev){
         ev.preventDefault();
         document.querySelector(".module__form").removeAttribute("hidden");
+    });
+
+    // hide search module
+    document.querySelector(".module__form").querySelector(".btn--close").addEventListener("click", function (ev){
+        ev.preventDefault();
+        document.querySelector(".module__form").setAttribute("hidden", "");
     });
 });
