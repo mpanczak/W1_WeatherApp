0ec39d1adec9e4a3891cf9cb5fac0df1e3f2684f
mpanczak <panczakmateusz@gmail.com>
2023-08-26 17:54:56 +0200

    show search module

--- js/app.js
+++ js/app.js
@@ -3,4 +3,8 @@ document.addEventListener("DOMContentLoaded", function (){
     fetch("http://api.weatherapi.com/v1/forecast.json?key=" + apiKey + "&q=auto:ip&days=5")
         .then(resp => resp.json().then(obj => console.log(obj))).catch(err => console.error(err));
 
+    document.getElementById("add-city").addEventListener("click", function (ev){
+        ev.preventDefault();
+        document.querySelector(".module__form").removeAttribute("hidden");
+    });
 });
